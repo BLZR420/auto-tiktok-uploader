5082874034c2b4d8ba55b373a9221914051d20a8
BLZR420 <blazee420@web.de>
2025-10-19 13:10:37 +0200

    Update tiktok-callback.js

--- api/tiktok-callback.js
+++ api/tiktok-callback.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { query } = req; // contains ?code=...&state=...
 
   // 1) HIER deine NEUE Make-Webhook-URL (Production) einsetzen
-  const makeWebhook = "https://hook.eu2.make.com/f0b0veoesc03nmhyspeernqtn0ybaq8t?secret=mySuperSecret123";
+  const makeWebhook = "https://hook.eu2.make.com/f0b0veoesc03nmhyspeernqtn0ybaq8t";
 
   // 2) Secret MUSS mit der Filter-Prüfung in Make übereinstimmen
   const SECRET = "mySuperSecret123";
